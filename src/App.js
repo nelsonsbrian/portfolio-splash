9b2c72e8984c3b8d5ceb636e392bdb4f1775a55c
Brian Nelson <nelsonsbrian@gmail.com>
2020-02-26 12:36:30 -0800

    add react-spring animations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
-import Header from './components/Header';
 import Body from './components/Body';
 
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,50 @@
 import React, { useState } from 'react';
-
 import { Link } from 'react-router-dom';
+import { Transition, Trail } from 'react-spring/renderprops'
+import { useTrail, animated } from 'react-spring'
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const Branding = () => {
+
+  function Branding() {
     return (
-      <div className="header-branding">
+      <div className={`menu-branding menu-${menuOpen}`}>
         <div className="portrait"></div>
       </div>
     )
   }
 
-  const Nav = () => {
+  const links = [
+    <Link className="nav-link" to="/">Home</Link>,
+    <Link to="/about" className="nav-link">About</Link>,
+    <Link to="/projects" className="nav-link">Projects</Link>,
+  ];
+  const slideRight = useTrail(links.length, {
+    from: { transform: 'translate3d(600px, 0, 0)' },
+    to: { transform: 'translate3d(0, 0, 0)' }
+  })
+
+  function Nav() {
     return (
-      <ul className="header-nav">
-        <li className="nav-item current">
-          <Link className="nav-link" to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">About</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/projects" className="nav-link">Projects</Link>
-        </li>
+      <ul className={`menu-nav menu-${menuOpen}`}>
+        {slideRight.map((props, index) => {
+          return (
+            <animated.div
+              key={index}
+              style={props}
+              className="box"
+            >
+              <li style={props} className={`nav-item menu-${menuOpen}`}>
+                {links[index]}
+              </li>
+            </animated.div>
+          );
+        })}
       </ul>
     )
   }
+
 
   return (
     <nav className="header">
@@ -38,10 +55,28 @@ const Header = () => {
         <div className="btn-line"></div>
       </div>
 
-      {menuOpen ? <Branding /> : null}
-      {menuOpen ? <Nav /> : null}
-
-
+      <Transition
+        items={menuOpen}
+        from={{ transform: 'translate3d(0, -100%, 0)' }}
+        enter={{ transform: 'translate3d(0, 0, 0)' }}
+        leave={{ transform: 'translate3d(0, -100%, 0)' }}>
+        {show => show && (props =>
+          <div style={props} className={`menu menu-${menuOpen}`}>
+            <Branding />
+          </div>
+        )}
+      </Transition>
+      <Transition
+        items={menuOpen}
+        from={{ transform: 'translate3d(0, 100%, 0)' }}
+        enter={{ transform: 'translate3d(0, 0, 0)' }}
+        leave={{ transform: 'translate3d(0, 100%, 0)' }}>
+        {show => show && (props =>
+          <div style={props} className={`menu menu-${menuOpen}`}>
+            <Nav />
+          </div>
+        )}
+      </Transition>
 
     </nav>);
 }
