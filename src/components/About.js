72dfdf6f67f7cc767485d2b1a4cb987ee947cc16
Brian Nelson <nelsonsbrian@gmail.com>
2020-03-22 18:46:01 -0700

    add education components to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { JobData } from '../data/jobs';
+import { EducationData } from '../data/education';
 
 const JobCard = ({ job }) => {
   const { company, title, points, technologies, image } = job;
@@ -30,6 +31,46 @@ const JobCard = ({ job }) => {
   );
 }
 
+const EducationCard = ({ education }) => {
+  const { company, title, description, technologies, image, items } = education;
+  const imagePath = image ? `/img/${image}` : `https://via.placeholder.com/100`;
+  return (
+    <div className="job-card">
+      <div className="job-header">
+        <h3 className="job-title">{company}</h3>
+        <div className="job-img-container">
+          <img src={imagePath}
+            alt={`${company} logo`}
+          />
+        </div>
+        <h6 className="job-position">{title}</h6>
+      </div>
+      <div className="job-content">
+        <div className="job-technologies">
+          {technologies.map(tech => <span key={tech} className="technology">{tech}</span>)}
+        </div>
+        {description &&
+          <div className="job-description">
+            {description}
+          </div>
+        }
+
+        {items.map(({ name, points }) => (
+          <div className="job-item" key={name}>
+            <h5 className="job-subtitle secondary">{name}</h5>
+            <ul className="job-points">
+              {points.map((point, i) => (
+                <li key={i}>{point}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
 export default function About() {
   return (
     <div id="about">
@@ -47,11 +88,15 @@ export default function About() {
           <p> As an experienced technologist with nearly a decade of coding scripts and creating end-consumer interfaces, I love the process of delivering high quality projects that leave team members and consumers with a need satisfied. I am looking for a role where I can utilize my technical and team building skills to use and further absorb newer technologies.</p>
         </div>
 
+        <h3 className="bio-heading text-secondary-color">Experience</h3>
         <div className="job">
           {JobData.map(job => <JobCard key={job.company} job={job} />)}
         </div>
 
-
+        <h3 className="bio-heading text-secondary-color">Education</h3>
+        <div className="job">
+          {EducationData.map(education => <EducationCard key={education.company} education={education} />)}
+        </div>
       </div>
     </div>
 
--- src/data/education.js
+++ src/data/education.js
@@ -5,7 +5,7 @@ export const EducationData = [
     description: "Epicodus offers full-time courses in languages such as C#, JavaScript, Ruby, and more. Every track starts with Introduction to Programming and includes an advanced JavaScript course, as well as two language-specific courses — four five-week sessions in total. In addition, students complete two weeks of job and internship prep and finish their studies with a five-week internship.",
     items: [
       {
-        name: "Skills Learned",
+        name: "Skills Learned:",
         points: [
           "Intro to Programming: HTML, CSS, JavaScript and GitHub.",
           "C#, .Net Core, Basic MVC web apps, Karma & Jasmin testing, MySQL database",
@@ -20,24 +20,24 @@ export const EducationData = [
   {
     company: "Central Washington University",
     title: "Web Development",
-    description: "Epicodus offers full-time courses in languages such as C#, JavaScript, Ruby, and more. Every track starts with Introduction to Programming and includes an advanced JavaScript course, as well as two language-specific courses — four five-week sessions in total. In addition, students complete two weeks of job and internship prep and finish their studies with a five-week internship.",
+    description: "",
     items: [
       {
-        name: "Majored: Business Administration",
+        name: "Major: Business Administration",
         points: [
           "Supply Chain Management Specialization",
           "Logistics and Inventory Management.",
         ],
       },
       {
-        name: "Minored: Mathematics",
+        name: "Minor: Mathematics",
         points: [
           "Calculus I, II, III, and IV",
           "Linear Algebra and Finite Math"
         ],
       },
       {
-        name: "Minored: Family Studies",
+        name: "Minor: Family Studies",
         points: [
           "Working with small groups and creating an environment that breeds communication and teamwork",
         ],
